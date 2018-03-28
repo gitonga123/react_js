3e5d68966e9d48be0cef5f28d932be41df06605f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-03-28 10:21:26 +0300

    Rest Parameters

--- functions_1.js
+++ functions_1.js
@@ -1,3 +1,4 @@
+"use strict";
 //ECMAScript 5
 function makeRequest(url,timeout, callback){
     timeout = (typeof timeout !== "undefined") ? timeout : 2000;
@@ -43,4 +44,31 @@ console.log(add1(5));
 console.log(add1(5,6));
 
 //The call to add1(undefined, 1) will trhough an error
+//TDZ - Temporal Dead Zone
 
+//Working with Unnamed Parameters
+
+//Rest Parameters -- is indicated by three dots(...)
+//preceding a named parameter
+//anything passed after the three dots is converted into an array
+
+function pick(object,...keys){
+    let result = Object.create(null);
+
+    for(let i = 0; i <= keys.length; i ++){
+        result[keys[i]] = object[keys[i]];
+    }
+
+    return result;
+}
+
+let book = {
+    title: "Understanding ECMASCript 2016",
+    author: "Nicholasi C. Zakasi",
+    year: 2016
+};
+
+let bookData = pick(book,"author","year");
+
+console.log(bookData.author);
+console.log(bookData.year);
