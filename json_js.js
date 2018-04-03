1b093790180a8fa7444ac1c128f4c522f5291e81
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-04-03 09:55:57 +0300

    JavaScript Object Notation

---
+++ json_js.js
@@ -0,0 +1,18 @@
+//JSON format for storing and transporting data
+//JSON = JavaScript Object Notation
+// /ritten as name/value pairs, object properties
+
+let text = '{ "employees" : [' +
+    '{ "firstName":"John" , "lastName":"Doe" },' +
+    '{ "firstName":"Anna" , "lastName":"Smith" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+//JavaScript has a built in function to convert a string, written in JSON format, into native JavaScript objects:
+//convert a JavaScript object into a string JSON.stringify().
+
+let text_obj = JSON.parse(text);
+
+console.log(text_obj);
+
+document.getElementById("demo").innerHTML = text_obj.employees[2].firstName;
+
+//Come Back Latter on this time time
