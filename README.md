9bdc7dcc208f75c24a7d6d272a0fee51c4a5792f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-04-04 09:27:14 +0300

    Node.js Event Loop -> watching files

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ React - Component, JSX
 ECMAScript 2015 -> ES6 ES2015
 BABEL - Allow us to write new ES2015 code and perform a tasks ->Transpiling (Convert the ES6 Code into an earlier version of JavaScript that has greater browser support)
 
+Node.js
+
 1. let and const
 2. Template Literals
 3. Arrow Functions
---
+++ watcher.js
@@ -0,0 +1,10 @@
+//Node.js
+//Access a file system
+//reading, writing, renaming and deleting files
+//creating useful and asynchronous file utilities
+//Architecture and Core
+//Patterns -> callbacks for handling asynchronous events
+//JavaScriptisms -> JavaScript features and best practises
+//supporting Code -> spawn and interact with child processes
+//capture their output and detect state changes
+
