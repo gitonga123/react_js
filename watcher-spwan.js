d75a0375ef9223e06558ace09c84622a84b8f146
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-04-04 10:21:10 +0300

    Capturing Data from an EventEmitter

--- watcher-spwan.js
+++ watcher-spwan.js
@@ -1,15 +1,16 @@
 ////Spawn a process in Node
-"use strict"
-const fs = require('fs'),
-spawn = require('child_process').spawn,
-filename = process.argv[2];
+"use strict";
+const 
+    fs = require('fs'),
+    spawn = require('child_process').spawn,
+    filename = process.argv[2];
 
-if(!filename){
+if (!filename) {
     throw Error("A file to watch must be specified");
 }
 
 fs.watch(filename, function(){
-    let ls = spawn('ls', [' -lh',filename]);
+    let ls = spawn('ls', ['-lh', filename]);
     ls.stdout.pipe(process.stdout);
 });
 
