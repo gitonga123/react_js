31b9691aab81c0cde9ac078faa5a4d56e26ab85f
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-04-04 09:35:57 +0300

    Watching a File For Changes

--- watcher.js
+++ watcher.js
@@ -8,3 +8,18 @@
 //supporting Code -> spawn and interact with child processes
 //capture their output and detect state changes
 
+//Asynchronous Coding/Programming -> Parallel Programming
+//fs => File System
+
+//require -> to use a Node.js Module
+//Module -> is a self-contained bit of JavaScript that provides 
+//functionality to be used elsewhere
+//The harmony -> tells node to use the latest ECMAScript features available
+
+const fs = require('fs');
+
+fs.watch('target.txt', function(){
+    console.log("File 'target.txt ' just changed");
+});
+
+console.log("Now watching target.txt for changes ...");
