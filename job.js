1d490c2b9d9e6b91e19f30e16ba78577521f7a44
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-06-11 16:11:43 +0300

    Modular Events

--- job.js
+++ job.js
@@ -1,16 +1,16 @@
 const EventEmitter = require('events')
 class Job extends EventEmitter {
-    constructor(ops) {
-        super(ops)
-        this.on('start', ()=>{
-            this.process()
-        })
-    }
-    process() {
-        setTimeout(()=>{
-            this.emit('done', {completedOn: new Date() })
-        }, 700)
-    }
+  constructor (ops) {
+    super(ops)
+    this.on('start', () => {
+      this.process()
+    })
+  }
+  process () {
+    setTimeout(() => {
+      this.emit('done', {completedOn: new Date()})
+    }, 700)
+  }
 }
 
-module.exports = job
+module.exports = Job
