3f45b6a01f6d6f853b27f83fe0609b5b4b51f4c9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-06-18 12:06:48 +0300

    HTTP Server with core http

---
+++ server1.js
@@ -0,0 +1,8 @@
+// HTTP Server with core http
+const http = require('http')
+const port = 8089
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Tpe': 'text/plain'})
+  res.end('Hello World\n')
+}).listen(port)
