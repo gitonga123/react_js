d9aa0017c455fca79584cc90fa18896d6e2b8fd2
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-06-18 12:09:50 +0300

    HTTP Server with core http

--- server1.js
+++ server1.js
@@ -6,3 +6,5 @@ http.createServer((req, res) => {
   res.writeHead(200, {'Content-Tpe': 'text/plain'})
   res.end('Hello World\n')
 }).listen(port)
+
+console.log(`Server Running at http://localhost:${port}`)
