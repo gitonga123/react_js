46ef59f6fc21dc51caee3093b5371cd60b601f4e
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-18 13:02:27 +0300

    Server Module

--- web-app/server.js
+++ web-app/server.js
@@ -1,8 +1,12 @@
 var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
-
-console.log('Server has started');
+function start() {
+  function onRequest(request, response) {
+    console.log('Request Received.');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+}
+exports.start = start
