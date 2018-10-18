3a4598c14fa346613c87d19b198adbee03e7a2ad
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-18 13:52:10 +0300

    Blocking and Non-blocking

--- web-app/requestHandlers.js
+++ web-app/requestHandlers.js
@@ -1,11 +1,22 @@
-function start () {
+var exec = require('child_process').exec;
+
+function start (response) {
   console.log('Start Function');
-  return 'Hello Start';
+  var content = 'empty';
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload () {
+function upload (response) {
   console.log('Upload Function');
-  return 'Hello Upload';
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello Upload');
+  response.end();
+  
 }
 
 exports.start = start;
--- web-app/server.js
+++ web-app/server.js
@@ -5,10 +5,10 @@ function start (route, handle) {
   function onRequest (request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request For' + pathname + ' received');
-    var content = route(handle, pathname);
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
+    // response.writeHead(200, {'Content-Type': 'text/plain'});
+    // response.write(content);
+    // response.end();
   }
   http.createServer(onRequest).listen(8888);
   console.log('Server has started');
