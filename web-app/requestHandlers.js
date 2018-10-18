7d82624d0ec33d598f9d41f7436ae1eff4d91633
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-18 13:38:13 +0300

    Handling Request - Router.js

--- web-app/requestHandlers.js
+++ web-app/requestHandlers.js
@@ -1,9 +1,11 @@
 function start () {
   console.log('Start Function');
+  return 'Hello Start';
 }
 
 function upload () {
   console.log('Upload Function');
+  return 'Hello Upload';
 }
 
 exports.start = start;
--- web-app/server.js
+++ web-app/server.js
@@ -5,9 +5,9 @@ function start (route, handle) {
   function onRequest (request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request For' + pathname + ' received');
-    route(handle, pathname);
+    var content = route(handle, pathname);
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
+    response.write(content);
     response.end();
   }
   http.createServer(onRequest).listen(8888);
