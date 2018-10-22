f81f04c58f6dd1c7fee92dbe730918add32c4e14
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-22 13:38:36 +0300

    Integers via Math.floor(), Math.ceil(), and Math.round()

--- objects_1.js
+++ objects_1.js
@@ -8,7 +8,20 @@ var jane = {
 
 eval('jane');
 
-// console.log(jane.name);
-// jane.name = 'John';
-// console.log(jane.name);
-// console.log(jane.describe());
+console.log(jane.name);
+jane.name = 'John';
+console.log(jane.name);
+console.log(jane.describe());
+
+// Math.floor()
+// converts its argument to the closest lower integer
+let number_1 = 3.2;
+console.log(number_1);
+// Math Floor function
+console.log(Math.floor(number_1));
+
+// Math Ceil converts its argument to the closest higher integer
+console.log(Math.ceil(number_1));
+
+// Math Round converts its argument ot the closet integer
+console.log(Math.round(number_1));
