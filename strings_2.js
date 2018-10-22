6ebcfa1a8cd475f94afa37c0d63d7a590dbb5018
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-22 13:52:48 +0300

    string conversion

--- strings_2.js
+++ strings_2.js
@@ -17,3 +17,12 @@ console.log(str2 === 'Written over multiple lines'); // true
 // Character Access
 console.log('abc'.charAt(1)); //b
 console.log('abc'[1]); //b
+
+// to string conversion
+let hi = 123;
+
+console.log(typeof hi);
+console.log(typeof hi.toString());
+console.log(typeof String(hi));
+let hi_2 = '123';
+console.log(typeof hi_2);
