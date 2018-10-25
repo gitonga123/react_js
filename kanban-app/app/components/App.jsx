47ce186225a1aaad2d800c7da9f1f8866cd1e988
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-25 11:25:46 +0300

    Adding Notes

--- kanban-app/app/components/App.jsx
+++ kanban-app/app/components/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import uuid from 'uuid';
 import Notes from './Notes';
 
-export default() => (
-  <div>
-    <button onClick={() => console.log('add note')}> + </button>
-    <Notes />
-    </div> 
-);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
+  render() {
+    const {notes} = this.state;
+
+    return(
+      <div>
+        <button onClick={() => console.log('add note')}> +</button>
+        <Note notes={notes} />
+      </div>
+    );
+  }
+}
--- kanban-app/app/components/Notes.jsx
+++ kanban-app/app/components/Notes.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
-import uuid from 'uuid';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learning Testing'
-  }
-];
-console.log(notes);
-export default() => (
+export default({notes}) => (
   <ul>
     {notes.map(note => <li key={note.id}>{note.task}</li>)}
   </ul>
