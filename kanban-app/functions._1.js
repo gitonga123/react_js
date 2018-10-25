4844e0bb0b079e0a655d5cd770b3093b1fc2344b
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-25 13:00:09 +0300

    Default Parameter Expressions

--- kanban-app/functions._1.js
+++ kanban-app/functions._1.js
@@ -7,3 +7,15 @@ function makeRequest(url, timeout = 2000, callback = function (){}) {
 }
 
 makeRequest();
+console.log(url_1); // throws an error, TDZ
+// Default Parameter Expressions
+function getValue() {
+  return 5;
+}
+
+function add(first, second = getValue()) {
+  return first + second;
+}
+
+console.log(add(1, 1)); // 2
+console.log(add(1)); // 6
