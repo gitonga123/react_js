1bf561a75d82ddbb94e76a0c4bce4291783f79b1
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-01 13:17:43 +0300

    Stashed

--- learn_node/nairobi.js
+++ learn_node/nairobi.js
@@ -1,16 +1,28 @@
 var express = require('express');
 
 var app = express();
-
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+
+var fortunes = [
+  'Conquer your fears or they will conquer you.',
+  'Rivers need springs.',
+  'Do not fear what you don\'t know.',
+  'You will have a pleasant surprise.',
+  'Whenever possible, keep it simple.',
+]
 app.use(express.static(__dirname + '/public/'));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3030);
-
+app.use(function (request, response, next) {
+  response.locals.showTests = app.get('env') !== 'production' && request.query.test === '1'
+});
 app.get('/', function (request, response) {
-  response.render('home');
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  console.log(randomFortune);
+
+  response.render('home', {fortune: randomFortune});
 });
 
 app.get('/about', function (request, response) {
@@ -29,6 +41,6 @@ app.use(function (error, request, response, next) {
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Express started on localhost:3030' + app.get('port') +
+  console.log('Express started on localhost:' + app.get('port') +
    ' ; press Ctrl-C to terminate.');
 });
