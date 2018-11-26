bfc654a53444f3a84f670e7c68ab482ad8bff2a9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-26 14:53:35 +0300

    Handling Click Events

---
+++ clicker.js
@@ -0,0 +1,14 @@
+// var num = 0;
+
+// document.getElementById('clickbutton').addEventListener('click', function(){
+// 	num++;
+// 	document.getElementById("clickbutton").innerHtml= num;
+// });
+
+
+var value = 0;
+
+function handleClick() {
+	value++;
+	document.getElementById("clickbutton").innerHTML = value;
+}
